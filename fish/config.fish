f68052c67ae04ecf2fbc3f5a58f66e1e4dd64f2e
lominoss-git <79030093+lominoss-git@users.noreply.github.com>
2021-09-17 11:18:13 +0100

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+if set -q VIRTUAL_ENV
+  echo -n
+end
+
+fm6000 -os "Arch, btw" -phb
