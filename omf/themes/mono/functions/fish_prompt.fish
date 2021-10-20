56a322ef59571f2b87f1bfbb8027c19628a2e337
lominoss-git <79030093+lominoss-git@users.noreply.github.com>
2021-10-20 19:06:24 +0100

    Update fish_prompt.fish

--- omf/themes/mono/functions/fish_prompt.fish
+++ omf/themes/mono/functions/fish_prompt.fish
@@ -4,7 +4,7 @@ function fish_prompt
 
   # Just calculate these once, to save a few cycles when displaying the prompt
   if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    set -g __fish_prompt_hostname (echo $hostname)
   end
   if not set -q __fish_prompt_char
     switch (id -u)
@@ -14,33 +14,8 @@ function fish_prompt
 	set -g __fish_prompt_char 'λ'
     end
   end
-
-  # Setup colors
-  #use extended color pallete if available
-#if [[ $terminfo[colors] -ge 256 ]]; then
-#    turquoise="%F{81}"
-#    orange="%F{166}"
-#    purple="%F{135}"
-#    hotpink="%F{161}"
-#    limegreen="%F{118}"
-#else
-#    turquoise="%F{cyan}"
-#    orange="%F{yellow}"
-#    purple="%F{magenta}"
-#    hotpink="%F{red}"
-#    limegreen="%F{green}"
-#fi
-  set -l normal (set_color normal)
-  set -l white (set_color 2A2A2A)
-  set -l turquoise (set_color 2A2A2A)
-  set -l orange (set_color 2A2A2A)
-  set -l hotpink (set_color 2A2A2A)
-  set -l blue (set_color 2A2A2A)
-  set -l limegreen (set_color 2A2A2A)
-  set -l purple (set_color 2A2A2A)
  
   # Configure __fish_git_prompt
-  set -l cmd_duration $CMD_DURATION
   set -g __fish_git_prompt_char_stateseparator ' '
   set -g __fish_git_prompt_color 5fdfff
   set -g __fish_git_prompt_color_flags df5f00
@@ -54,17 +29,7 @@ function fish_prompt
   set -l current_user (whoami)
 
   # Line 1
-  echo -n $white'╭─'$hotpink$current_user$white' at '$orange$__fish_prompt_hostname$white' in '$limegreen(pwd|sed "s=$HOME=⌁=")$turquoise
-  __fish_git_prompt " (%s)"
-  echo
-
-  # Line 2
-  echo -n $white'╰─'
-  # support for virtual env name
-  if set -q VIRTUAL_ENV
-      echo -n "($turquoise"(basename "$VIRTUAL_ENV")"$white)"
-  end
-  echo -n $white'─'$__fish_prompt_char $normal
+  # echo -e '\033[1m'$current_user' at '$__fish_prompt_hostname' in '(pwd | sed "s=$HOME=~=g")
+  echo -e (pwd | sed "s=$HOME=~=g")
+  echo -e '\033[1m→\033[0m '
 end
-
-
